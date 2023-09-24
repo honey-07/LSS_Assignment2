7fd3a82f85f96b97afc12ef632e4d9de314965f2
HONEY PATEL <123378526+honey-07@users.noreply.github.com>
2023-09-24 17:34:44 +0530

    Update que4.cpp

--- que4.cpp
+++ que4.cpp
@@ -1,1 +1,18 @@
+echo "Enter hour from 1-24 :"
+read hr
+echo "Enter min : "
+read min
+
+if [ $hr -lt 12 -a $min -le 60 ]
+then
+	echo "GOOD MORNING!!!"
+elif [ $hr -lt 17 -a $hr -ge 12  -a $min -le 60 ]
+then
+	echo "GOOD AFTERNOON!!!"
+elif  [ $hr -lt 20 -a $hr -ge 17  -a $min -le 60 ]
+then
+	echo "GOOD EVENING!!!"
+else
+	echo "GOOD NIGHT!!!"
+fi
 
