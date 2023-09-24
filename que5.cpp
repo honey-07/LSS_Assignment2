dad74e36c3a2075f5531ccee9d33d61650bb2177
HONEY PATEL <123378526+honey-07@users.noreply.github.com>
2023-09-24 17:35:14 +0530

    Update que5.cpp

--- que5.cpp
+++ que5.cpp
@@ -1,1 +1,26 @@
+echo "Enter number : "
+read n
+flag=0
+i=2
+if [ $n -gt 0 ]
+then
+	while [ $i -lt $n ]
+	do
+	 if [ `expr $n % $i` -eq 0 ]
+		then
+			flag=1
+			break
+	fi
+	i=`expr $i + 1`
+
+	done
+fi
+
+if [ $flag -eq 0 ]
+then
+	echo "$n is prime number"
+
+else
+	echo "$n is not prime number"
+fi
 
