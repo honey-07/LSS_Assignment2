0c4211407d434f5d649d66712a17509efa4e053c
HONEY PATEL <123378526+honey-07@users.noreply.github.com>
2023-09-24 17:31:54 +0530

    Update que1.cpp

--- que1.cpp
+++ que1.cpp
@@ -1,1 +1,17 @@
+echo "enter number of rows : "
+read r
+
+i=1
+while [ $i -le $r ]
+do
+	j=1
+	while [ $j -le $i ]
+	do
+	echo -n " $j"
+	j=`expr $j + 1`
+	done
+echo "\n"
+i=`expr $i + 1`
+done
+
 
